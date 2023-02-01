63588cd8c4c1f1b161e852587b6c3bd444c5ada5
spezza19 <shailen.chander@me.com>
2023-01-31 20:55:10 -0500

    improved team boost bar styling, now adjusts styling for current target

--- src/lib/PlayerBoostBar.svelte
+++ src/lib/PlayerBoostBar.svelte
@@ -23,9 +23,17 @@
         height: 85%;
         width: 100%;
         border-radius: 5px;
+
+        background: rgb(23,25,31);
+        background: linear-gradient(90deg, rgba(23,25,31,1) 44%, rgba(30,32,39,1) 93%, rgba(34,37,44,1) 100%);
+        outline:3px solid rgb(5 5 5 / 0.1);
     }
 
-    .container.blue {
+    .container.target {
+        outline: 3px solid rgba(48, 46, 60, 0.5);
+    }
+
+    /* .container.blue {
         background: rgb(38,69,171);
         background: linear-gradient(100deg, rgba(38,69,171,1) 0%, rgba(32,79,169,1) 60%, rgb(75, 93, 164) 100%); 
     }
@@ -33,7 +41,7 @@
     .container.orange {
         background: rgb(171,80,38);
         background: linear-gradient(260deg, rgba(171,80,38,1) 0%, rgba(165,79,22,1) 64%, rgba(197,152,54,1) 100%); 
-    }
+    } */
 
     .boost {
         position: absolute;
@@ -43,7 +51,15 @@
         left: 4%;
         background-color: rgba(40 40 40 / 0.8);
 
-        transition: all ease 0.5;
+        transition: all ease 0.3;
+    }
+
+    .blue .boost.fill {
+        background-color: rgba(69, 88, 230, 0.5);
+    }
+
+    .orange .boost.fill {
+        background-color: rgba(255, 102, 0, 0.5);
     }
 
     .target .boost.fill {
@@ -57,14 +73,14 @@
     .name {
         margin: 0 auto;
         width: 92%;
-        color: rgb(250 250 250 / 0.75);
+        color: rgb(250 250 250 / 0.5);
 
-        transition: all 500ms ease;
+        transition: all 0.3 ease;
     }
 
     .target .name {
         color: rgb(250 250 250 / 1);
-        transition: all 500ms ease;
+        transition: all 0.3 ease;
 
     }
 
